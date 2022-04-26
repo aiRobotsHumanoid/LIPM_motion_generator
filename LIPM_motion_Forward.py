Fix: init_pose and cmd sign

LIPM_motion_Forward.py
@@ -88,8 +88,8 @@ class LIPM_motion:
 
         """ 機器人基礎姿態設定 """
         # Set Initial Pose
-        self.initR = [0,  0.0,   0.4, -0.8,  0.4,  0.0] 
-        self.initL = [0,  0.0,   0.4, -0.8,  0.4,  0.0]
+        self.initR = [0,  0,   -0.4, -0.8,  0.4,  0] 
+        self.initL = [0,  0,    0.4,  0.8,  0.4,  0]
         # Legs : Legs link lenghth (mm)
         self.Legs = np.array([102.5, 170, 175, 70.33, 34.7])/1000
 
@@ -202,7 +202,7 @@ class LIPM_motion:
         Lean_angleR = np.array(Lean_angleR) * Deg2Rad
         Lean_angleL = np.array(Lean_angleL) * Deg2Rad
 
-        # ## turn angle ##	
+        ## turn angle ##	
         DesiredTheta_R1 = []
         DesiredTheta_L1 = []        
         

LIPM_motion_T_L.py
@@ -105,8 +105,8 @@ class LIPM_motion:
 
         """ 機器人基礎姿態設定 """
         # Set Initial Pose
-        self.initR = [0,  0.0,   0.4, -0.8,  0.4,  0.0] 
-        self.initL = [0,  0.0,   0.4, -0.8,  0.4,  0.0]
+        self.initR = [0,  0,   -0.4, -0.8,  0.4,  0] 
+        self.initL = [0,  0,    0.4,  0.8,  0.4,  0]
         # Legs : Legs link lenghth (mm)
         self.Legs = np.array([102.5, 170, 175, 70.33, 34.7])/1000
 

source/command_generator.py
@@ -4,13 +4,29 @@ from math import *
 import time
 import csv
 
+
+
 class Cmd_Generator:
     def __init__(self, motion_file, w_file):
 
+        """ Presetting """
+
         self.r_file = motion_file
         self.w_file = w_file
-        self.add_Dynamixel_data = True
+
+        # 用哪些馬達
+        self.add_Dynamixel_data = True # True False
         self.add_Haitai_data = [30, 32, 33, 34, 40, 42, 43, 44] 
+
+        if len(self.add_Haitai_data) == 4:
+            self.frame_range = 6
+        elif len(self.add_Haitai_data) == 8:
+            self.frame_range = 11
+        print(f"len of add_Haitai_data = {len(self.add_Haitai_data)}")
+
+        # 每個timestep間隔
+        self.time_step = 0.1
+
 
         with open(self.r_file, 'r', newline='') as r_f:
             self.datas = pd.read_csv(r_f, header=None)
@@ -21,32 +37,37 @@ class Cmd_Generator:
         self.mode_list = np.ones([len(self.datas),len(self.datas[0])]) # 0: pos of this step is same as pos of last step
         self.vel_list = np.ones([len(self.datas),len(self.datas[0])])
 
-    def mode_scan(self):
+    def data_scan(self):
+        """
+            @brief: Scan the datas to record the Haitai mode and Dyn velocity
+
+            @param mode_list - Haitai mode
+            @param vel_list  - Dyn velocity
+        """
         for frame_idx, frame in enumerate(self.datas):
-            for list_idx in range(len(frame)):
+            for list_idx in range(self.frame_range):
                 motordata = frame[list_idx]
                 
-                # Motorid
+                ## Motorid ##
                 motorid = self.motor_list[list_idx]
 
-                # Position command
-                if motorid == 32 or motorid == 42 :
-                    p_cmd = np.round(-motordata, 2)
-                else: p_cmd = np.round(motordata, 2)
+                ## Position command ##
+                p_cmd = np.round(motordata, 2)
                 
-                # Set HT03 mode 0 (situation:first,last, the same command)
+                ## Set HT03 mode 0 (situation:first,last, the same command) ##
                 if frame_idx==0 or frame_idx==len(self.datas)-1:
                     self.mode_list[frame_idx][list_idx] = 0
 
                 elif p_cmd==self.pre_pos[list_idx]:
                     self.mode_list[frame_idx-1][list_idx] = 0
 
-                # Calculate Dynamixel velocity
+                ## Calculate Dynamixel velocity ##
                 if  self.add_Dynamixel_data == True:
                     if frame_idx == 0:
                         time_cmd = 2
                     else:
                         time_cmd = 0.1
+
                     if motorid in [31, 41]: # Pro100
                         scaled = 2920 / 175.2  # (max/ (deg/sec))
                         self.vel_list[frame_idx][list_idx] = np.round((p_cmd - self.pre_pos[list_idx]) / time_cmd * scaled)
@@ -64,9 +85,16 @@ class Cmd_Generator:
         return self.mode_list, self.vel_list
 
     def write_cmd(self):
-        mode_list, vel_list = self.mode_scan()
+        """ 
+            Data Format : 
+            HaiTai       : [motortype, motorid, mode, p_cmd, time_cmd, t_ff, kp, kd]
+            Dynamixel    : [motortype, motorid, p_cmd, vel]
+            Wait Command : [motortype, wait_time]
 
-        """ Data Format : [motortype, motorid, mode, p_cmd, time_cmd, t_ff, kp, kd] """
+            @param command_list - All motor cmd
+        """
+        mode_list, vel_list = self.data_scan()
+
         command_list = []
         for frame_idx, frame in enumerate(self.datas):
             if frame_idx == 0:
@@ -76,13 +104,13 @@ class Cmd_Generator:
                     command_list.append(command)
 
             # frame_range = len(frame)-6 # right leg
-            for list_idx in range(len(frame)):
-            # for list_idx in range(len(frame)):
+            for list_idx in range(self.frame_range):
                 motordata = frame[list_idx]
 
-                # Motorid
+                ## Motorid ##
                 motorid = self.motor_list[list_idx]
 
+                ## motortype ##
                 # Right Leg
                 if motorid == 30 or motorid == 32 or motorid == 33 or motorid == 34:
                     motortype = 3
@@ -94,33 +122,33 @@ class Cmd_Generator:
                     motortype = 5
 
 
-                # Velocity (just for Dynamixel)
+                ## Velocity (just for Dynamixel) ##
                 vel = vel_list[frame_idx][list_idx]
                 
-                # Mode (just for haitai)
+                ## Mode (just for haitai) ##
                 mode = mode_list[frame_idx][list_idx]
 
-                # Position command
-                if motorid == 31 or motorid == 32  or motorid == 35 or motorid == 41 or motorid == 43 or motorid == 45: 
+                ## Position command (check +/-) ##
+                if motorid == 31 or motorid == 35 or motorid == 41 or motorid == 45: 
                     p_cmd = np.round(-motordata, 2)
                 else: p_cmd = np.round(motordata, 2)
 
-                # Time command (sec)
+                ## Time command (sec) ##
                 if frame_idx == 0:
                     time_cmd = 2 
-                else: time_cmd = 0.1
+                else: time_cmd = self.time_step
 
-                # Torque command
+                ## Torque command ##
                 t_ff = 0
 
-                # Kp command, if the motion is jumping, we need to set a smaller kp 
+                ## Kp command, if the motion is jumping, we need to set a smaller kp ##
                 if frame_idx >= 0 :
                     kp = 100
                 
-                # Kd command
+                ## Kd command ##
                 kd = 0.2
 
-                # Write Command
+                """ Write Command """
                 if frame_idx==len(self.datas)-1:
                     if motorid == 3 or motorid == 31 or motorid == 35 or motorid == 41 or motorid == 45:
                         command = [motortype, motorid, p_cmd, vel]
@@ -138,12 +166,14 @@ class Cmd_Generator:
                         command = [motortype, motorid, p_cmd, vel]
                         if self.add_Dynamixel_data == True:
                             command_list.append(command)
+
                     elif motorid in self.add_Haitai_data:
                         command = [motortype, motorid, mode, p_cmd, time_cmd, t_ff, kp, kd]
                         command_list.append(command)
                 self.pre_pos[list_idx] = p_cmd
             
-            # Wait Command = [motortype, wait_time]
+
+            ## Wait Command = [motortype, wait_time] ##
             wait_command = [100, time_cmd*pow(10, 6)]
             command_list.append(wait_command)
         ending_command = [100, 3*pow(10, 6)]  # c++ usleep(1) = 10^-6 sec

source/motion_function.py
@@ -117,21 +117,17 @@ def modifiable_foot_generation(dt, Amp, T, T_DSP, BoolPlot):
     N = len(Amp[0])
 
     if a == 3:
-        lenght_t = math.floor(N*T/dt)
         t1 = T_DSP / 2
         t2 = T - T_DSP / 2
         T_SSP = T - T_DSP
         biasx = 0
         biasy = 0
-        i = 0
         t_index = 1
         n_index = 1	
         t_real = 0
-        Foot_x_y_z = []
         size_t_temp = N * T / dt
 
         for k in range(1, int(size_t_temp)+1):
-            Foot_x_y_z = []
             t = t_index * dt
             Ampx = Amp[0][n_index - 1]
             Ampy = Amp[1][n_index - 1]
@@ -144,9 +140,6 @@ def modifiable_foot_generation(dt, Amp, T, T_DSP, BoolPlot):
             t_index = t_index + 1
             t_real = t_real + dt
 
-            # Foot_x_y_z.append(temp_x)
-            # Foot_x_y_z.append(temp_y)
-            # Foot_x_y_z.append(temp_z)
             vec_sum.append([temp_x, temp_y, temp_z])
 
             if t_real > n_index*T:
@@ -164,13 +157,17 @@ def OutputMotion(initR, initL, Rup, T, sampleT, dt, hip, Legs, thetaR_length,\
     hip_r = hip[0]
     hip_l = hip[1]
     samplek = sampleT / dt
-    d2 = 43.42 / 1000 #48.25
+    d2 = 43.42 / 1000  # motor35/45 ＆ 地面的距離
     ####################################
     thetaR_leg = []
     thetaL_leg = []
-    PR_sample = []
-    PL_sample = []
 
+    """ 
+        @brief  Motion Planning : Calculate Inverse Kinetic for all motor
+
+        @param  thetaR_leg - planned motion for right leg
+        @param  thetaL_leg - planned motion for right leg
+    """
     for i in range(1, thetaR_length+1):
         index_i = int(i * samplek)
         
@@ -204,29 +201,27 @@ def OutputMotion(initR, initL, Rup, T, sampleT, dt, hip, Legs, thetaR_length,\
         else:
             if Rup:
                 thetaR_leg[i - 1][1] = hip_r * thetaR_leg[i - 1][1]
-                # thetaL_leg[i - 1][1] = hip_l * 1 * thetaL_leg[i - 1][1]
-                # if (i == thetaR_length):
-                #     thetaL_leg[i - 1][2] = thetaL_leg[i - 1][2]
-                # else:
-                #     thetaL_leg[i - 1][2] = thetaL_leg[i - 1][2] * 1	
             else:	
                 thetaL_leg[i - 1][1] = hip_l * thetaL_leg[i - 1][1]													
 
-    MScaleR_Leg_rows = []
-    MScaleL_Leg_rows = []
+
+    """
+        @brief  Calculate motor scaled and offset in Sim and Real
+
+        @param  ScaledthetaX_leg_test - motor data for simulation
+        @param  Motor_ScaledthetaX_leg_test - motor data for real world
+    """
     size = 6, thetaR_length
-    # ScaledthetaR_leg = np.zeros(size, np.float32)
-    # ScaledthetaL_leg = np.zeros(size, np.float32) 
     ScaledthetaR_leg_test = np.zeros(size, np.float64)
     ScaledthetaL_leg_test = np.zeros(size, np.float64)
     Motor_ScaledthetaR_leg_test = np.zeros(size, np.float64)
     Motor_ScaledthetaL_leg_test = np.zeros(size, np.float64)
 
-
     # Joint angle: radian -> motor resolution
+    # For Sim scaled (rad to rad)
     MScaleR_Leg = [1, 1, 1, 1, 1, 1]
     MScaleL_Leg = [1, 1, 1, 1, 1, 1]
-
+    # For Real scaled (rad to HaiTai/Dyn)
     Motor_MScaleR_Leg = [1, 180/math.pi, 1, 1, 1, 180/math.pi]
     Motor_MScaleL_Leg = [1, 180/math.pi, 1, 1, 1, 180/math.pi]
 
@@ -237,40 +232,46 @@ def OutputMotion(initR, initL, Rup, T, sampleT, dt, hip, Legs, thetaR_length,\
     MotorOffsetL = []	
 
     # motor rotation +/-
-    Motor_Dir_R = [1, 1,-1,-1,-1,1]
-    Motor_Dir_L = [1, 1,-1,-1,-1,1]
+    Motor_Dir_R = [1, 1,  1,-1, -1, 1]
+    Motor_Dir_L = [1, 1, -1, 1, -1, 1]
     for j in range(6):
+        """ Calculate motor initial offset  """
+        """ @param  Offset - Like bias, how far are the planned motion(in time 0) and the Initial pose  """
+        # For Sim
         MotorOffsetR.append(initR[j] - Motor_Dir_R[j] * thetaR_leg[0][j])
         MotorOffsetL.append(initL[j] - Motor_Dir_L[j] * thetaL_leg[0][j])
-        # print(f"MotorOffsetL = {MotorOffsetL}")
+        # For Real
         Motor_MotorOffsetR.append(initR[j] - Motor_Dir_R[j] * thetaR_leg[0][j])
         Motor_MotorOffsetL.append(initL[j] - Motor_Dir_L[j] * thetaL_leg[0][j])
-    # for i in range(thetaR_length):
-    #     MScaleR_Leg.append(MScaleR_Leg_rows)
-    #     MScaleL_Leg.append(MScaleL_Leg_rows)
-    #     Motor_MScaleR_Leg.append(MScaleR_Leg_rows)
-    #     Motor_MScaleL_Leg.append(MScaleL_Leg_rows)
-    # print(f"MScaleR_Leg = {MScaleR_Leg}")
-    # print(f"Motor_MScaleR_Leg = {Motor_MScaleR_Leg}")
     
     for i in range(thetaR_length):
+        """ Calculate the motor data with scaled and offset """
         for j in range(6):
+            # For Sim   
             ScaledthetaR_leg_test[j][i] = MotorOffsetR[j] + thetaR_leg[i][j] * MScaleR_Leg[j] * Motor_Dir_R[j]
             ScaledthetaL_leg_test[j][i] = MotorOffsetL[j] + thetaL_leg[i][j] * MScaleL_Leg[j] * Motor_Dir_L[j]
-            Motor_ScaledthetaR_leg_test[j][i] = thetaR_leg[i][j] * Motor_MScaleR_Leg[j] * Motor_Dir_R[j] + Motor_MotorOffsetR[j] 
-            Motor_ScaledthetaL_leg_test[j][i] = thetaL_leg[i][j] * Motor_MScaleL_Leg[j] * Motor_Dir_L[j] + Motor_MotorOffsetL[j] 
+            # For Real
+            Motor_ScaledthetaR_leg_test[j][i] = (Motor_MotorOffsetR[j] + thetaR_leg[i][j] * Motor_Dir_R[j]) * Motor_MScaleR_Leg[j]
+            Motor_ScaledthetaL_leg_test[j][i] = (Motor_MotorOffsetL[j] + thetaL_leg[i][j] * Motor_Dir_L[j]) * Motor_MScaleL_Leg[j]
+
+            # if i == 0: # 應該要跟 init pose(rad/s) 相同 => 修改 Motor_Dir_R / Motor_Dir_L
+            #     print(f"{Motor_ScaledthetaR_leg_test[j][i]/Motor_MScaleR_Leg[j]} = ({Motor_MotorOffsetR[j]} + {thetaR_leg[i][j]} * {Motor_Dir_R[j]})")
+            #     print(f"{Motor_ScaledthetaL_leg_test[j][i]/Motor_MScaleL_Leg[j]} = ({Motor_MotorOffsetL[j]} + {thetaL_leg[i][j]} * {Motor_Dir_L[j]})")
             if j == 0:
                 Motor_ScaledthetaR_leg_test[j][i] = thetaR_leg[i][j] * Motor_MScaleR_Leg[j] * Motor_Dir_R[j]
                 Motor_ScaledthetaL_leg_test[j][i] = thetaL_leg[i][j] * Motor_MScaleL_Leg[j] * Motor_Dir_L[j] 
                 # print(f"{Motor_ScaledthetaR_leg_test[j][i]} = {thetaR_leg[i][j]} * {Motor_MScaleR_Leg[j]} * {Motor_Dir_R[j]}")
     
-    # Back to initial pose
+    """ FORCE the robot back to initial pose in the end """
     for i in range(6):
+        # For Sim
         ScaledthetaR_leg_test[i][thetaR_length-1] = ScaledthetaR_leg_test[i][0]
         ScaledthetaL_leg_test[i][thetaR_length-1] = ScaledthetaL_leg_test[i][0]
+        # For Real
         Motor_ScaledthetaR_leg_test[i][thetaR_length-1] = Motor_ScaledthetaR_leg_test[i][0]
         Motor_ScaledthetaL_leg_test[i][thetaR_length-1] = Motor_ScaledthetaL_leg_test[i][0]
 
+    """ For Sim, let the robot be stable after walking """
     # add more stable step 
     # for i in range(5):
     #     ScaledthetaR_leg_test = np.append(ScaledthetaR_leg_test, ScaledthetaR_leg_test[i][0])
@@ -279,12 +280,17 @@ def OutputMotion(initR, initL, Rup, T, sampleT, dt, hip, Legs, thetaR_length,\
         # Motor_ScaledthetaL_leg_test = np.append(Motor_ScaledthetaL_leg_test, Motor_ScaledthetaL_leg_test[i][0])
     
 
+    """ Collect motor data """
+    # For Sim
     motor_job_data =  np.concatenate((ScaledthetaR_leg_test, ScaledthetaL_leg_test), axis=0)
-    Motor_test_data = np.concatenate((Motor_ScaledthetaR_leg_test, Motor_ScaledthetaL_leg_test), axis=0)
     motor_job_data = np.round(motor_job_data.T, 3)
-    Motor_test_data = np.round(Motor_test_data.T, 3)
     for i in range(30):
         motor_job_data = np.append(motor_job_data, [motor_job_data[-1]], axis=0)
+
+    # For Real
+    Motor_test_data = np.concatenate((Motor_ScaledthetaR_leg_test, Motor_ScaledthetaL_leg_test), axis=0)
+    Motor_test_data = np.round(Motor_test_data.T, 3)
+    
     # print(f'motor_job_data = {len(motor_job_data)}')
     return motor_job_data , Motor_test_data
 